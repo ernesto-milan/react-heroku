54efff9225c2283b372ccd70fee6be15fd705d21
Mondora <mondora@MacBook-Pro-di-Mondora.local>
2020-01-16 11:29:16 +0100

    basic table

--- src/App.js
+++ src/App.js
@@ -1,26 +1,152 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {AppProvider, Page} from '@shopify/polaris';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+
+    const headings = [
+      'Product name',
+      'SKU',
+      'Stock quantity',
+      'Wholesale cost',
+      'Sale price',
+      'Quantity sold',
+      'Gross sales',
+      'Net sales',
+      'Notes',
+    ];
+
+    const rows = [
+      [
+        'Red and black plaid scarf with thin red stripes and thick black stripes',
+        124689325,
+        28,
+        '$35.00',
+        '$60.00',
+        12,
+        '$720.00',
+        '$300.00',
+        '',
+      ],
+      [
+        'Yellow plaid scarf',
+        124689389,
+        0,
+        '$35.00',
+        '$60.00',
+        20,
+        '$1200.00',
+        '$500.00',
+        'Currently on back order by the supplier. Do not place another order to restock.',
+      ],
+      [
+        'Blue plaid scarf',
+        124689332,
+        30,
+        '$35.00',
+        '$60.00',
+        10,
+        '$600.00',
+        '$250.00',
+        '',
+      ],
+      [
+        'Pink plaid scarf',
+        124689376,
+        16,
+        '$35.00',
+        '$60.00',
+        4,
+        '$240.00',
+        '$100.00',
+        '',
+      ],
+    ];
+
+    return (
+      <AppProvider>
+        <Page title="Data table">
+          <DataTable headings={headings} rows={rows} />
+        </Page>
+      </AppProvider>
+    );
+  }
 }
 
 export default App;
+
+
+
+export class DataTable extends React.Component {
+  renderHeadingRow = (_cell, cellIndex) => {
+    const {headings} = this.props;
+
+    return (
+      <Cell
+        key={`heading-${cellIndex}`}
+        content={headings[cellIndex]}
+        header={true}
+      />
+    )
+  };
+  
+  renderRow = (_row, rowIndex) => {
+    const {rows} = this.props;
+
+    return (
+      <tr key={`row-${rowIndex}`}>
+        {rows[rowIndex].map((_cell, cellIndex) => {
+          return (
+            <Cell
+              key={`${rowIndex}-${cellIndex}`}
+              content={rows[rowIndex][cellIndex]}
+            />
+          )
+        })}
+      </tr>
+    )
+  };
+
+  render() {
+    const {headings, rows} = this.props;
+
+    this.renderHeadingRow = this.renderHeadingRow.bind(this);
+    this.renderRow = this.renderRow.bind(this);
+    
+    const theadMarkup = (
+      <tr key="heading">
+        {headings.map(this.renderHeadingRow)}
+      </tr>
+    );
+
+    const tbodyMarkup = rows.map(this.renderRow);
+  
+    return (
+      <table className="Table">
+        <thead>{theadMarkup}</thead>
+        <tbody>{tbodyMarkup}</tbody>
+      </table>
+    );
+  }
+}
+
+
+
+export function Cell({
+  content,
+  header,
+}) {
+
+  const cellMarkup = header ? (
+    <th className="Cell Cell-header">
+      {content}
+    </th>
+  ) : (
+    <td className="Cell">
+      {content}
+    </td>
+  );
+
+  return (cellMarkup);
+}
